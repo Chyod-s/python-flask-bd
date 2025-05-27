4e28d31f5e1154b8ec6dda6b2ccd002a0b8a3539
Klayton COD.ias <65176457+Ton-Chyod-s@users.noreply.github.com>
2025-05-27 14:16:21 -0400

    feat: adicionar campos de e-mail, nome e confirmação de senha no formulário de registro

--- src/api_main/web_assets/static/js/user/register/scripts.js
+++ src/api_main/web_assets/static/js/user/register/scripts.js
@@ -7,9 +7,17 @@ document.getElementById('register-form').addEventListener('submit', async (e) =>
     e.preventDefault();
     const msgEl = document.getElementById('message'); 
     const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirm-password').value;
+    const name = document.getElementById('name').value;
 
-    const data = { user_name: username, password: password };
+    const data = { 
+        user_name: username,
+        email: email, 
+        password: password,
+        confirm_password: confirmPassword,
+        name: name };
 
     try {
         const response = await fetch('/api/register', {
